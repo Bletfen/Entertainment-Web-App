5e8cbcbbebb8a79292f9a080851aa4f962991c1d
Giorgi <giorgigrigolishvili@gmail.com>
2025-11-07 00:42:46 +0400

    bookmarkcontroller on carrousel tweak

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import MovieDetails from "./MovieDetails";
+import BookmarkController from "./BookmarkController";
 
 interface ICarouselProps {
   slides: TTrendingProps;
@@ -26,13 +27,14 @@ export const EmblaCarousel: React.FC<ICarouselProps> = ({
           <div
             className="flex-shrink-0 w-[24rem] md:w-[40%] 
             lg:w-[30%] pt-[7rem] pl-[1.6rem] pr-[0.8rem] pb-[1.6rem]
-            bg-center bg-cover rounded-[0.8rem]"
+            bg-center bg-cover rounded-[0.8rem] relative"
             key={index}
             style={{
               backgroundImage: `url(${movie.thumbnail.trending?.small})`,
             }}
           >
             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/15 z-0"></div>
+            <BookmarkController />
             <div className="relative z-10">
               <MovieDetails movie={movie} />
             </div>
