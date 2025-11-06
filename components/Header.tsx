485860f3da784c11ec2f20596a6781ef0f788bc7
Giorgi <giorgigrigolishvili@gmail.com>
2025-11-06 19:50:19 +0400

    header finished

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,17 @@ import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <div>
-      <p className="text-[36px]">hello</p>
+    <div
+      className="flex justify-between items-center
+      p-[1.6rem]"
+    >
+      <img src="/assets/logo.svg" alt="logo-icon" />
       <NavBar />
+      <img
+        src="/assets/image-avatar.png"
+        alt="avatar"
+        className="w-[2.4rem] h-[2.4rem]"
+      />
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,22 @@
+"use client";
 import { navigationArray } from "@/navigationArray";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 export default function NavBar() {
+  const pathName = usePathname();
+  console.log(pathName);
   return (
-    <div>
+    <div className="flex items-center gap-[2.4rem]">
       {navigationArray.map((item) => {
         const Icon = item.icon;
+        const isActive = pathName === item.navigate;
         return (
           <Link key={item.navigate} href={item.navigate} className="group">
-            <Icon className="text-[#5a698f] group-hover:text-white transition-colors" />
+            <Icon
+              className={`group-hover:text-white transition-colors
+                ${isActive ? "text-white" : "text-[#5a698f]"}`}
+            />
           </Link>
         );
       })}
